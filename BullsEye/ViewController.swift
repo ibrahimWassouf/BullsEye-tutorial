123f38f1ae18b826ac0e36a47fd44ca979ee7bce
Ibrahim Wassouf <ib981347@dal.ca>
2022-10-12 21:46:18 -0300

    added connections for all labels

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
     var currentValue: Int = 1
-    @IBOutlet var slider: UISlider!
+    var score: Int = 0
     var targetValue: Int = 0
+    var roundNumber: Int = 0
     @IBOutlet var targetLabel: UILabel!
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var roundLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +26,12 @@ class ViewController: UIViewController {
 
     
     @IBAction func showAlert() {
-        let message = "The value of the slide is: \(currentValue)" + "\nThe target value is: \(targetValue)"
+        let difference = abs(currentValue - targetValue)
+        let points = 100 - difference
+        let message = "You scored \(points) points"
+        score += points
+        
+        
         let alert = UIAlertController(title: "Hello World",
                                   message: message,
                                       preferredStyle: .alert)
@@ -43,11 +52,14 @@ class ViewController: UIViewController {
     func startNewRound(){
         targetValue = Int.random(in:1...100)
         currentValue = Int(slider.value)
+        roundNumber += 1
         updateLabels()
     }
     
     func updateLabels() {
         targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(roundNumber)
     }
     
 }
