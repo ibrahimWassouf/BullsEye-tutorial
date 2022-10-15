bfde1378669862bd80ee706c22844b9e2a024d84
Ibrahim Wassouf <ib981347@dal.ca>
2022-10-14 22:43:27 -0300

    attempted layout constraints

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Ibrahim Wassouf on 2022-10-14.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func closeView(){
+        dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
